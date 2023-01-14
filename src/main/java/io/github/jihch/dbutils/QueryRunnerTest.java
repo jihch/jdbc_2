0bfadf0331b73fe51cffc2c091b754164ed42cdc
jihch <haoqibaobaoji@126.com>
2023-01-14 15:03:01 +0800

    Update QueryRunnerTest.java

--- src/main/java/io/github/jihch/dbutils/QueryRunnerTest.java
+++ src/main/java/io/github/jihch/dbutils/QueryRunnerTest.java
@@ -5,13 +5,11 @@ import io.github.jihch.bean.Customer;
 import io.github.jihch.betterutil.JDBCUtils;
 import org.apache.commons.dbutils.QueryRunner;
 import org.apache.commons.dbutils.ResultSetHandler;
-import org.apache.commons.dbutils.handlers.BeanHandler;
-import org.apache.commons.dbutils.handlers.BeanListHandler;
-import org.apache.commons.dbutils.handlers.MapHandler;
-import org.apache.commons.dbutils.handlers.MapListHandler;
+import org.apache.commons.dbutils.handlers.*;
 import org.junit.Test;
 
 import java.sql.Connection;
+import java.sql.Date;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
@@ -140,8 +138,8 @@ public class QueryRunnerTest {
     }
 
     /**
-     * MapListHandler:是 ResultSetHandler 接口的实现类，对应表中的一条记录
-     * 将字段及相应字段的值作为 Map 中的 key 和 value
+     * MapListHandler:是 ResultSetHandler 接口的实现类，对应表中的多条记录
+     * 将字段及相应字段的值作为 Map 中的 key 和 value，将这些 map 添加到 List 中
      */
     @Test
     public void testQueryMapList() {
@@ -170,6 +168,72 @@ public class QueryRunnerTest {
         }
 
         list.forEach(System.out::println);
+
+    }
+
+    /**
+     * ScalarHandler:是 ResultSetHandler 接口的实现类，对应查询结果中的单个值
+     */
+    @Test
+    public void testQueryObjectLong() {
+        Connection conn = null;
+        String sql = "select count(*) from customers";
+
+        /*
+        AbstractKeyedHandler, AbstractListHandler, ArrayHandler, ArrayListHandler, BaseResultSetHandler,
+        BeanHandler, BeanListHandler, BeanMapHandler, ColumnListHandler,
+        KeyedHandler, MapHandler, MapListHandler, ScalarHandler
+         */
+        ScalarHandler rsh = new ScalarHandler();
+        Long count = null;
+        QueryRunner runner = new QueryRunner();
+
+        try {
+
+            conn = JDBCUtils.getConnectionFromDruid();
+
+            count = (Long) runner.query(conn, sql, rsh);
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            JDBCUtils.closeResource(conn, null);
+        }
+
+        System.out.println(count);
+
+    }
+
+    /**
+     * ScalarHandler:是 ResultSetHandler 接口的实现类，对应查询结果中的单个值
+     */
+    @Test
+    public void testQueryObjectDate() {
+        Connection conn = null;
+        String sql = "select max(birth) from customers";
+
+        /*
+        AbstractKeyedHandler, AbstractListHandler, ArrayHandler, ArrayListHandler, BaseResultSetHandler,
+        BeanHandler, BeanListHandler, BeanMapHandler, ColumnListHandler,
+        KeyedHandler, MapHandler, MapListHandler, ScalarHandler
+         */
+        ScalarHandler rsh = new ScalarHandler();
+        Date d = null;
+        QueryRunner runner = new QueryRunner();
+
+        try {
+
+            conn = JDBCUtils.getConnectionFromDruid();
+
+            d = (Date) runner.query(conn, sql, rsh);
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            JDBCUtils.closeResource(conn, null);
+        }
+
+        System.out.println(d);
 
     }
 
